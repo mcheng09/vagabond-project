9d8459553bd82d05dca6ba2eeae5f80a6fb8db99
Kate Nelson <mkatenelson@gmail.com>
2016-06-20 14:42:25 -0700

    no changes, changing branches

--- vagabond_app/db/seeds.rb
+++ vagabond_app/db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.create({first_name: "Anthony", last_name: "Schurz", email: "anthonyschurz@gmail.com", location: "San Francisco"},
 {first_name: "Wayne", last_name: "Banks", email: "waynbanks13@gmail.com", location: "San Francisco"})
