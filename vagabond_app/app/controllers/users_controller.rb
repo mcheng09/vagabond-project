e34ae1492084566180248e28de8f8ee3b474014b
Michael Cheng <mcheng.oc@gmail.com>
2016-06-20 11:43:12 -0700

    adds new and create to route and controller

--- vagabond_app/app/controllers/users_controller.rb
+++ vagabond_app/app/controllers/users_controller.rb
@@ -5,5 +5,25 @@ class UsersController < ApplicationController
     render :index
   end
 
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:users).permit(:first_name, :last_name, :email, :password)
+  end
+
 
 end
