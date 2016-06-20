67f45d9f47ab9ec70c548a653a70565ceccf0aae
Kate Nelson <mkatenelson@gmail.com>
2016-06-20 14:04:04 -0700

    create sessions controller and sign in form

--- vagabond_app/app/controllers/application_controller.rb
+++ vagabond_app/app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  
   protect_from_forgery with: :exception
+
+  def login(user)
+      session[:user_id] = user.id
+      @current_user = user
+    end
+
+    def current_user
+      @current_user ||= User.find(session[:user_id])
+    end
+
+    def logged_in?
+      if current_user == nil
+        redirect_to "/sign_in"
+      end
+    end
+
+    def logout
+      @current_user = session[:user_id] = nil
+    end
+
+
 end
--- vagabond_app/config/routes.rb
+++ vagabond_app/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   resources :posts
+
+  get "/signin", to: "sessions#new"
+
 end
