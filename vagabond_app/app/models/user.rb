7b224096d4154c7f1575672d0b23ae6f92cd7e2d
Michael Cheng <mcheng.oc@gmail.com>
2016-06-21 17:00:33 -0700

    add validations for email and posts

--- vagabond_app/app/models/user.rb
+++ vagabond_app/app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates :email, presence: true, uniqueness: true, format: { with: /@/}
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
