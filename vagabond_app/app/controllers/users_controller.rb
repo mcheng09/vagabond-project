beb474f60c7ad71960977dbe760b76a58f7d5290
Michael Cheng <mcheng.oc@gmail.com>
2016-06-22 17:14:06 -0700

    truncates profile posts to 1000 max char

--- vagabond_app/app/controllers/users_controller.rb
+++ vagabond_app/app/controllers/users_controller.rb
@@ -21,6 +21,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
+    @city = City.find(params[:id])
     render :show
   end
 
